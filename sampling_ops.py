6a82a6946e12897657210a212223a705cc2eb66e
Patrick Jennewein <patrick.jennewein2@gmail.com>
2024-09-30 11:35:20 -0500

    Added ability to dynamically downsample in task #1

--- sampling_ops.py
+++ sampling_ops.py
@@ -1,31 +1,45 @@
 import numpy as np
 import cv2
 
+
 def downsample_image(image, downsample_rate):
-    """Down-sample the image by removing alternate columns and rows"""
+    """down-sample the image by removing columns and rows"""
     new_image = image[::downsample_rate, ::downsample_rate]
-    cv2.imshow("#1a: Down-sampled Image (half size)", new_image)
+
+    # created a resized window
+    window_name = "#1a: Down-sampled Image"
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_name, new_image.shape[1], new_image.shape[0])
+
+    # display the down-sampled image
+    cv2.imshow(window_name, new_image)
+
     return new_image
 
 
 def upsample_image(image, downsample_rate):
-    """Up-sample the image by copying pixels"""
-    # Get the dimensions of the original image
+    """Up-sample the image by copying pixels, dynamically based on the down-sample rate."""
+
+    # get the dimensions of the original image
     height, width = image.shape[:2]
 
-    # Create a new empty image of double the size
+    # create a new, empty image of the correct up-sampled size
     upsampled_image = np.zeros((height * downsample_rate,
                                 width * downsample_rate,
                                 image.shape[2]),
                                dtype=image.dtype)
 
-    # copy pixels
+    # copy pixels to a larger area
+    """
+    Each original pixel will fill an area with the size of the downsample_rate.
+    For example, if the downsample_rate is 2, the old 1 x 1 space will now
+    fill a 2 x 2 space, decreasing the overall quality of the upsampled image.
+    This uses the NumPy slice to achieve this easily.  
+    """
     for row in range(height):
         for col in range(width):
-            upsampled_image[downsample_rate * row, downsample_rate * col] = image[row, col]
-            upsampled_image[downsample_rate * row + 1, downsample_rate * col] = image[row, col]
-            upsampled_image[downsample_rate * row, downsample_rate * col + 1] = image[row, col]
-            upsampled_image[downsample_rate * row + 1, downsample_rate * col + 1] = image[row, col]
+            upsampled_image[row * downsample_rate:(row + 1) * downsample_rate,
+                            col * downsample_rate:(col + 1) * downsample_rate] = image[row, col]
 
     cv2.imshow("#1b: Up-sampled Image (original size)", upsampled_image)
     return upsampled_image
