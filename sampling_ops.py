a4d70120f312c1fafcbdb5b15f197d9363614110
Patrick Jennewein <patrick.jennewein2@gmail.com>
2024-10-03 09:00:05 -0500

    Finished procedure and 2

--- sampling_ops.py
+++ sampling_ops.py
@@ -3,7 +3,7 @@ import cv2
 
 
 def show_steps(image, depth, mode='decreasing'):
-    """Show the steps of sampling either in decreasing or increasing order"""
+    """show the steps of sampling either in decreasing or increasing order"""
     if mode == 'decreasing':
         for i in range(1, depth + 1):
             print(f"Downsampling to {int(image.shape[0] / 2 ** i)} x {int(image.shape[1] / 2 ** i)}...")
@@ -13,30 +13,25 @@ def show_steps(image, depth, mode='decreasing'):
             print(f"Upsampling to {int(image.shape[0] * (2 ** i))} x {int(image.shape[1] * (2 ** i))}...")
         print()
 
+
 def downsample_image_by_removal(image, downsample_rate, depth):
     """down-sample the image by removing columns and rows"""
-
-    # show the steps of downsampling
     show_steps(image, depth)
 
-    # downsample
+    # downsample by excluding every n'th row/column
     new_image = image[::downsample_rate, ::downsample_rate]
 
-    # created a resized window
+    # create a new window to display
     window_name = f"{new_image.shape[0]} x {new_image.shape[1]} by Removal"
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(window_name, new_image.shape[1], new_image.shape[0])
-
-    # display the down-sampled image
     cv2.imshow(window_name, new_image)
 
     return new_image
 
 
 def upsample_image_by_copying(image, downsample_rate, depth):
-    """Up-sample the image by copying pixels, dynamically based on the down-sample rate."""
-
-    # show the steps of upsampling
+    """Up-sample the image by copying pixels"""
     show_steps(image, depth, mode='increasing')
 
     # get the dimensions of the original image
@@ -60,6 +55,7 @@ def upsample_image_by_copying(image, downsample_rate, depth):
             upsampled_image[row * downsample_rate:(row + 1) * downsample_rate,
                             col * downsample_rate:(col + 1) * downsample_rate] = image[row, col]
 
+    # create a new window to display
     window_name = f"{upsampled_image.shape[0]} x {upsampled_image.shape[1]} by Copying"
     cv2.imshow(window_name, upsampled_image)
     return upsampled_image
@@ -68,8 +64,6 @@ def upsample_image_by_copying(image, downsample_rate, depth):
 
 def downsample_image_by_averaging(image, downsample_rate, depth):
     """down-sample the image by averaging pixels."""
-
-    # show the steps of downsampling
     show_steps(image, depth)
 
     # calculate the new dimensions
@@ -96,12 +90,13 @@ def downsample_image_by_averaging(image, downsample_rate, depth):
 
 def upsample_image_by_interpolation(image, upsample_rate, depth):
     """upsample the image by linear interpolation."""
-
-    # show the steps of upsampling
     show_steps(image, depth, mode='increasing')
+
+    # increase size by factor of sample rate
     upsampled_image = cv2.resize(image, (image.shape[1] * upsample_rate, image.shape[0] * upsample_rate),
                                  interpolation=cv2.INTER_LINEAR)
 
+    # create a new window to display
     window_name = f"{upsampled_image.shape[0]} x {upsampled_image.shape[1]} by Interpolation"
     cv2.imshow(window_name, upsampled_image)
     return upsampled_image
@@ -109,9 +104,14 @@ def upsample_image_by_interpolation(image, upsample_rate, depth):
 
 
 def intensity_downsample(image, downsample_rate, depth):
+    """use bit shifting to intensity down-sample the image."""
+    # (image >> downsample_rate) dividing pixels by downsample_rate
+    # << downsample_rate shifts the bits that are remaining
     downsampled_image = (image >> downsample_rate) << downsample_rate
-    cv2.imshow(f'Downsampled Image by {downsample_rate} bits', downsampled_image)
 
+    # create a new window to display
+    window_name = f'Downsampled Image by {downsample_rate} bits'
+    cv2.imshow(window_name, downsampled_image)
     return downsampled_image
 
 
